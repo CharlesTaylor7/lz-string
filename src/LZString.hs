1d1c31a96a3beacbda2cd0ed7d27802674880582
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-10-18 16:46:56 -0400

    Work

--- src/LZString.hs
+++ src/LZString.hs
@@ -1,13 +1,31 @@
-module LZString where
+{-# LANGUAGE OverloadedStrings #-}
+module LZString (decompressBase64) where
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+
+-- Use an Intmap to mimic a js array
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as Map
+
+
+keyStrBase64 :: ByteString
+keyStrBase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+f = fromCharCode
+-- | from a list of character code points to a utf-16 encoded bytestring
+fromCharCode :: [Int] -> ByteString
+fromCharCode = undefined
+
+
+getBaseValue :: alphabet -> character -> Int
+getBaseValue = undefined
 
 
 decompressBase64 :: ByteString -> ByteString
 decompressBase64 = undefined
 
-_decompress :: () -> IO ByteString
-_decompress = undefined
+_decompress :: (Int, Int, Int -> Int) -> IO ByteString
+_decompress (length, resetValue, getNextValue) = undefined
 
 
